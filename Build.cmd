826310d145db75acb2c5939290580df73523e04e
Michael Mairegger <michael.mairegger@yahoo.de>
2014-04-15 15:26:48 +0200

    [Fix] Build.cmd had problems if the solutions path contained spaces.

--- Build.cmd
+++ Build.cmd
@@ -2,6 +2,6 @@
 
 set framework=v4.0.30319
 
-"%SystemDrive%\Windows\Microsoft.NET\Framework\%framework%\MSBuild.exe" %~dp0\XamlAttributeOrdering.sln /Property:Configuration=Release
+"%SystemDrive%\Windows\Microsoft.NET\Framework\%framework%\MSBuild.exe" "%~dp0\XamlAttributeOrdering.sln"/Property:Configuration=Release
 
-%~dp0\packages\NuGet.CommandLine.2.7.3\tools\NuGet.exe pack %~dp0\XamlAttributeOrdering\XamlAttributeOrdering\XamlAttributeOrdering.nuspec -Properties Configuration=Release -Version %1
+"%~dp0\packages\NuGet.CommandLine.2.7.3\tools\NuGet.exe" pack "%~dp0\XamlAttributeOrdering\XamlAttributeOrdering\XamlAttributeOrdering.nuspec" -Properties Configuration=Release -Version %1
